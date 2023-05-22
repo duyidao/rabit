25806d952b76680a3d3f11be5f4e7c0c535a4648
duyidao <2014567296@qq.com>
2023-05-22 23:06:09 +0800

    小兔鲜项目初始化

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+// createRouter：创建router实例对象
+// createWebHistory：创建history模式的路由
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  // path和component对应关系的位置
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/views/HomeView.vue'),
+    },
+  ],
+})
+
+export default router
