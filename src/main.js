e203d5ab157fbb7b46f22423a75491bf06a1e1d2
刀刀 <9022610+duyidao@user.noreply.gitee.com>
2023-05-24 23:41:10 +0800

    引入axios；封装接口函数并调用，测试结果成功

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import {test} from '@api/index'
+test().then(res => {
+  console.log(res);
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
