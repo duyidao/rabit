2303d0c7a02a81613c02c8c74851592ef7f58c11
刀刀 <9022610+duyidao@user.noreply.gitee.com>
2023-05-28 18:30:52 +0800

    新鲜好物与人气推荐相同模块封装为一个面板，通过prop与插槽动态设置具体内容

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -11,9 +12,15 @@ import homeProduct from './components/HomeProduct.vue'
     <HomeCategory />
     <HomeBanner />
   </div>
-  <HomeNew />
-  <HomeHot />
-  <homeProduct />
+  <!-- <HomeNew />
+  <HomeHot /> -->
+  <HomeProduct />
+  <HomePanel title="新鲜好物" subTitle="新鲜好物，好多商品">
+    <div>新鲜好物的插槽</div>
+  </HomePanel>
+  <HomePanel title="人气推荐" subTitle="人气推荐，猜你喜欢">
+    <div>人气推荐的插槽</div>
+  </HomePanel>
 </template>
 
 <style scoped>
