fdaa769ffcefcbc91e7706b6c9762758fae3709d
刀刀 <9022610+duyidao@user.noreply.gitee.com>
2023-05-28 12:00:38 +0800

    layout静态页面完成

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,12 +1,21 @@
-<script setup lang="ts">
-
+<script setup>
+import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
 </script>
 
 <template>
-  <h1 class="priceColor">layout</h1>
+  <!-- 头部 -->
+  <LayoutHeader></LayoutHeader>
+
+  <!-- 导航 -->
+  <LayoutNav></LayoutNav>
 
   <!-- 二级路由 -->
   <router-view></router-view>
+
+  <!-- 底部 -->
+  <LayoutFooter></LayoutFooter>
 </template>
 
 <style lang="scss" scoped>
