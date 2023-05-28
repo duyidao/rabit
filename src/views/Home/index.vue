967c71e7bff05177d85002f0ac60223a067c0131
刀刀 <9022610+duyidao@user.noreply.gitee.com>
2023-05-28 18:02:33 +0800

    首页模块拆分导入，轮播图左侧导航数据动态渲染完成

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts">
-
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
 </script>
 
 <template>
-  <h1>Hello vue3</h1>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
 </template>
 
 <style scoped>
