f30e4ed6b836176cb214216774a60e61f3cdc1c0
刀刀 <9022610+duyidao@user.noreply.gitee.com>
2023-05-28 18:17:27 +0800

    轮播图组件静态设置、接口获取动态数据并渲染实现

--- src/apis/layout.js
+++ src/apis/layout.js
@@ -1,7 +1,15 @@
 import http from '@/utils/http'
 
+// 获取导航数据
 export const getCategoryAPI = () => {
   return http({
     url: '/home/category/head'
+  })
+}
+
+// 获取轮播图数据
+export const getBannerAPI = () => {
+  return http({
+    url: '/home/banner'
   })
 }
