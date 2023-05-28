8bab5e3fd309b0ea5298faf3fc8c655002b2b677
刀刀 <9022610+duyidao@user.noreply.gitee.com>
2023-05-28 11:41:10 +0800

    引入静态资源：图片与样式

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-import {test} from '@api/index'
-test().then(res => {
-  console.log(res);
-})
+// 引入公共样式
+import '@/styles/common.scss'
 
 const app = createApp(App)
 
