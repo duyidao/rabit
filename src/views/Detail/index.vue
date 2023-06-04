c4c5797ab627d08d0c969a8052e146644226b359
刀刀 <9022610+duyidao@user.noreply.gitee.com>
2023-06-04 15:40:38 +0800

    商品详情页实现；渲染多层模板报错bug解决

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,6 +1,18 @@
 <script setup>
+import { ref, onMounted } from "vue";
+import {useRoute} from 'vue-router'
+import { getDetail } from "@api/detail";
 
+const route = useRoute()
+const goodData = ref({})
+const getDetailFn = async () => {
+  const res= await getDetail(route.params.id)
+  console.log(res);
+  goodData.value = res.result
+  console.log(goodData.value);
+}
 
+onMounted(() => getDetailFn())
 </script>
 
 <template>
@@ -9,11 +21,9 @@
       <div class="bread-container">
         <el-breadcrumb separator=">">
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item :to="{ path: '/' }">母婴
-          </el-breadcrumb-item>
-          <el-breadcrumb-item :to="{ path: '/' }">跑步鞋
-          </el-breadcrumb-item>
-          <el-breadcrumb-item>抓绒保暖，毛毛虫子儿童运动鞋</el-breadcrumb-item>
+          <el-breadcrumb-item :to="{ path: `/category/${goodData.categories?.[0].id}` }">{{ goodData.categories?.[0].name }} </el-breadcrumb-item>
+          <el-breadcrumb-item :to="{ path: `/category/${goodData.categories?.[1].id}` }">{{ goodData.categories?.[1].name }} </el-breadcrumb-item>
+          <el-breadcrumb-item>{{ goodData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
       <!-- 商品信息 -->
@@ -27,38 +37,38 @@
               <ul class="goods-sales">
                 <li>
                   <p>销量人气</p>
-                  <p> 100+ </p>
+                  <p>{{ goodData.salesCount }}</p>
                   <p><i class="iconfont icon-task-filling"></i>销量人气</p>
                 </li>
                 <li>
                   <p>商品评价</p>
-                  <p>200+</p>
+                  <p>{{ goodData.commentCount }}</p>
                   <p><i class="iconfont icon-comment-filling"></i>查看评价</p>
                 </li>
                 <li>
                   <p>收藏人气</p>
-                  <p>300+</p>
+                  <p>{{ goodData.collectCount }}</p>
                   <p><i class="iconfont icon-favorite-filling"></i>收藏商品</p>
                 </li>
                 <li>
                   <p>品牌信息</p>
-                  <p>400+</p>
+                  <p>{{ goodData.brand?.name }}</p>
                   <p><i class="iconfont icon-dynamic-filling"></i>品牌主页</p>
                 </li>
               </ul>
             </div>
             <div class="spec">
               <!-- 商品信息区 -->
-              <p class="g-name"> 抓绒保暖，毛毛虫儿童鞋 </p>
-              <p class="g-desc">好穿 </p>
+              <p class="g-name">{{ goodData.name }}</p>
+              <p class="g-desc">{{ goodData.desc }}</p>
               <p class="g-price">
-                <span>200</span>
-                <span> 100</span>
+                <span>{{ goodData.price }}</span>
+                <span> {{ goodData.oldPrice }}</span>
               </p>
               <div class="g-service">
                 <dl>
                   <dt>促销</dt>
-                  <dd>12月好物放送，App领券购买直降120元</dd>
+                  <dd>{{ goodData.name }}</dd>
                 </dl>
                 <dl>
                   <dt>服务</dt>
@@ -76,11 +86,8 @@
 
               <!-- 按钮组件 -->
               <div>
-                <el-button size="large" class="btn">
-                  加入购物车
-                </el-button>
+                <el-button size="large" class="btn"> 加入购物车 </el-button>
               </div>
-
             </div>
           </div>
           <div class="goods-footer">
@@ -93,20 +100,18 @@
                 <div class="goods-detail">
                   <!-- 属性 -->
                   <ul class="attrs">
-                    <li v-for="item in 3" :key="item.value">
-                      <span class="dt">白色</span>
-                      <span class="dd">纯棉</span>
+                    <li v-for="item in goodData.details?.properties" :key="item.value">
+                      <span class="dt">{{ item.name }}</span>
+                      <span class="dd">{{ item.value }}</span>
                     </li>
                   </ul>
                   <!-- 图片 -->
-
+                  <img v-for="(img, index) in goodData.details?.pictures" :key="index" :src="img" alt="">
                 </div>
               </div>
             </div>
             <!-- 24热榜+专题推荐 -->
-            <div class="goods-aside">
-
-            </div>
+            <div class="goods-aside"></div>
           </div>
         </div>
       </div>
@@ -114,8 +119,7 @@
   </div>
 </template>
 
-
-<style scoped lang='scss'>
+<style scoped lang="scss">
 .xtx-goods-page {
   .goods-info {
     min-height: 600px;
@@ -252,7 +256,7 @@
       flex: 1;
       position: relative;
 
-      ~li::after {
+      ~ li::after {
         position: absolute;
         top: 10px;
         left: 0;
@@ -306,7 +310,7 @@
       font-size: 18px;
       position: relative;
 
-      >span {
+      > span {
         color: $priceColor;
         font-size: 16px;
         margin-left: 10px;
@@ -340,14 +344,13 @@
     }
   }
 
-  >img {
+  > img {
     width: 100%;
   }
 }
 
 .btn {
   margin-top: 20px;
-
 }
 
 .bread-container {
