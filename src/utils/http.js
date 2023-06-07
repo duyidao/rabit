2689b860db23cb385250ac38d439ac354c3760b6
刀刀 <9022610+duyidao@user.noreply.gitee.com>
2023-06-07 22:56:26 +0800

    请求携带token请求头

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import { useUserStore } from '@/stores/user'
 
 const http = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net/',
@@ -9,12 +10,18 @@ const http = axios.create({
 
 // axios请求拦截器
 http.interceptors.request.use(config => {
+  const { userinfo } = useUserStore()
+  // 获取token
+  const token = userinfo.token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 }, e => Promise.reject(e))
 
 // axios响应式拦截器
 http.interceptors.response.use(res => res.data, e => {
-  ElMessage({type: 'error', message: e.response.data.message})
+  ElMessage({ type: 'error', message: e.response.data.message })
   return Promise.reject(e)
 })
 
