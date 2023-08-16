1056345c09eec2d0509c615c136fe9a78203565b
平安喜乐 <2014567296@qq.com>
2023-08-16 10:13:33 +0800

    feat: 轮播图组件封装完成，自动导入功能实现，组件类型添加完成

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { http } from '@/utils/http'
 import CustomNavBar from './components/CustomNavBar.vue'
+
+const bannerList = ref([])
+const getBannerFn = async () => {
+  const res = await http<unknown>({
+    method: 'GET',
+    url: '/home/banner',
+  })
+  console.log(res)
+  bannerList.value = res.result
+}
+
+onMounted(() => getBannerFn())
 </script>
 
 <template>
   <CustomNavBar />
+  <XtxSwiper :list="bannerList" />
 </template>
 
 <style></style>
