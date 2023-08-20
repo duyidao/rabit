051b127a21497fd440dbd1c6ba85e1741952c855
平安喜乐 <2014567296@qq.com>
2023-08-20 12:31:42 +0800

    feat: 登录模块真实手机快捷登录方法定义实现；模拟快捷登录接口调用实现，后续实现数据全局保存

---
+++ src/services/login.ts
@@ -0,0 +1,26 @@
+import { http } from '@/utils/http'
+
+// 手机快捷登录
+type LoginParams = {
+  code: string
+  encryptedData: string
+  iv: string
+}
+export const loginApi = (data: LoginParams) => {
+  return http<any>({
+    method: 'POST',
+    url: '/login/wxMin',
+    data,
+  })
+}
+
+// 小程序登录
+export const LoginMinApi = (phoneNumber: string) => {
+  return http<any>({
+    method: 'POST',
+    url: '/login/wxMin/simple',
+    data: {
+      phoneNumber,
+    },
+  })
+}
